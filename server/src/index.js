05b4bc82dadb50afcfb37729d35d62c8dd0cbb17
José Manuel Mármol Ávila <josemanuel.marmol@alu.ceacfp.es>
2026-03-16 10:08:59 +0100

    Servidor Express configurado y capa de servicios

---
+++ server/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const { PORT } = require('./config/env');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Servidor funcionando correctamente');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
---
+++ server/src/services/task.service.js
@@ -0,0 +1,32 @@
+let tasks = [];
+
+function obtenerTodas() {
+  return tasks;
+}
+
+function crearTarea(data) {
+  const nuevaTarea = {
+    id: Date.now().toString(),
+    title: data.title,
+    completed: false
+  };
+
+  tasks.push(nuevaTarea);
+  return nuevaTarea;
+}
+
+function eliminarTarea(id) {
+  const index = tasks.findIndex((task) => task.id === id);
+
+  if (index === -1) {
+    throw new Error('NOT_FOUND');
+  }
+
+  tasks.splice(index, 1);
+}
+
+module.exports = {
+  obtenerTodas,
+  crearTarea,
+  eliminarTarea
+};
